b87999506903b2e8ca633701f7d1befbcb54a0c7
mwigginsdev <89605282+mwigginsdev@users.noreply.github.com>
2022-02-13 21:14:22 -0600

    Create grader.py
    add grader

---
+++ grader.py
@@ -0,0 +1,83 @@
+import zipfile
+import argparse
+import os
+from os.path import isfile, join
+import shutil
+import subprocess
+import re
+
+
+def runTest(dir):
+    bashCommand = 'g++ ./tmp/test.cpp -o ./tmp/run --std=c++14'
+    process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
+    output, error = process.communicate()
+
+    bashCommand = './tmp/run'
+    process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
+    output, error = process.communicate()
+    print(error)
+    return output.decode("utf-8")
+
+
+def writeScore(zipName, score):
+    with open("grades.csv", "a") as myfile:
+        myfile.write(zipName + "," + score + ",\n")
+
+
+def writeError(zipName, error, files):
+    with open("errors.csv", "a") as myfile:
+        myfile.write(zipName + "," + error + "," + files + ",\n")
+
+
+workingDir = os.getcwd() + '/tmp'
+
+parser = argparse.ArgumentParser(description='Process hw submissions')
+parser.add_argument('path', help='path to folder of all hw zip files')
+parser.add_argument('tester', help='path to tester file')
+
+args = parser.parse_args()
+
+try:
+    shutil.rmtree(os.getcwd() + 'grades.csv')
+except:
+    print("no grades file")
+
+
+try:
+    shutil.rmtree(os.getcwd() + 'errors.csv')
+except:
+    print("no errors file")
+
+
+with open("grades.csv", "a") as myfile:
+    myfile.write("name, score,\n")
+
+with open("errors.csv", "a") as myfile:
+    myfile.write("name, error,files,\n")
+
+zips = os.listdir(args.path)
+
+for zip in zips:
+    print("PROCESSING: " + zip)
+    if ".zip" in zip:
+        try:
+            with zipfile.ZipFile(args.path + '/' + zip, 'r') as zip_ref:
+                os.mkdir(workingDir)
+                zip_ref.extractall(workingDir)
+                shutil.copy(args.tester, workingDir + '/test.cpp')
+
+                output = runTest(workingDir)
+                score = re.search("Tests: (.+?) out of 20", output)
+                writeScore(zip, score.group(1))
+        except Exception as e:
+            print("error running: ", zip)
+            print(e)
+            print(os.listdir(workingDir))
+            writeError(zip, str(e), str(os.listdir(workingDir)))
+
+    try:
+        shutil.rmtree(workingDir)
+    except:
+        print("missing workingDir")
+
+print("**************ALL DONE**************")
